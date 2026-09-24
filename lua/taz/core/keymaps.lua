5e2aac11cd09e50e3c8b8a2edcefd39cd4a74e58
BuzzingTaz <catzfgnam@gmail.com>
2026-09-25 01:15:20 +0530

    Add some simple keybinds for close all and force write

--- lua/taz/core/keymaps.lua
+++ lua/taz/core/keymaps.lua
@@ -1,28 +1,35 @@
+local map = vim.keymap.set
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
-vim.keymap.set({ 'n', 'x' }, 's', '<Nop>')
+map({ 'n', 'x' }, 's', '<Nop>')
 
-vim.keymap.set('n', '-', '<CMD>Oil<CR>', { silent = true, desc = 'Open parent directory' })
+map('n', '-', '<CMD>Oil<CR>', { silent = true, desc = 'Open parent directory' })
 
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, desc = 'Scroll down half page' })
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, desc = 'Scroll up half page' })
+map('n', '<C-d>', '<C-d>zz', { noremap = true, desc = 'Scroll down half page' })
+map('n', '<C-u>', '<C-u>zz', { noremap = true, desc = 'Scroll up half page' })
 
-vim.keymap.set('n', 'n', 'nzz', { noremap = true })
-vim.keymap.set('n', 'N', 'Nzz', { noremap = true })
+map('n', 'n', 'nzz', { noremap = true })
+map('n', 'N', 'Nzz', { noremap = true })
 
-vim.keymap.set('x', '<leader>p', '"_dP', { noremap = true, silent = true, desc = 'Paste without yanking' })
+map('x', '<leader>p', '"_dP', { noremap = true, silent = true, desc = 'Paste without yanking' })
 
-vim.keymap.set('n', '<up>', '<C-w>k', { noremap = true, desc = 'Move window up' })
-vim.keymap.set('n', '<down>', '<C-w>j', { noremap = true, desc = 'Move window up' })
-vim.keymap.set('n', '<left>', '<C-w>h', { noremap = true, desc = 'Move window up' })
-vim.keymap.set('n', '<right>', '<C-w>l', { noremap = true, desc = 'Move window up' })
+map('n', '<up>', '<C-w>k', { noremap = true, desc = 'Move window up' })
+map('n', '<down>', '<C-w>j', { noremap = true, desc = 'Move window up' })
+map('n', '<left>', '<C-w>h', { noremap = true, desc = 'Move window up' })
+map('n', '<right>', '<C-w>l', { noremap = true, desc = 'Move window up' })
+
+-- Quick esc
+map("i", "jj", "<Esc>", opts)
+
+map("n", "<leader>w", ":w!<CR>", opts)
+map("n", "<leader>q", ":qa<CR>", opts)
